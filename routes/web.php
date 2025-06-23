b96ffb174e397d82d1305e3207c3c59f8e4c66d7
hjrsmail <hijir512@gmail.com>
2025-06-23 10:53:40 +0800

    modified routes

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,29 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CounselorController;
 use App\Http\Controllers\GuideController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MaterialController;
+use App\Models\Counselor;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
-Route::get('/', [CommentController::class, 'index'])->name('home');
-
+// Route yang make controller
+Route::get('/', [HomeController::class, 'show' ])->name('home');
 Route::post('/ulasan', [CommentController::class, 'store']);
-
 Route::get('/panduan', [GuideController::class, 'show']);
+Route::get('/konselor', [CounselorController::class, 'show']);
+Route::get('/aktivitas', [ActivityController::class, 'show']);
+Route::get('/materi', [MaterialController::class, 'show']);
 
+// Assesment Route
 Route::get('/assesment-awal', function () {
     return Inertia::render('assesment/assesment-awal');
 });
 Route::get('/assesment-akhir', function () {
     return Inertia::render('assesment/assesment-akhir');
 });
-Route::get('/materi', function () {
-    return Inertia::render('materi-page');
-});
-
 
